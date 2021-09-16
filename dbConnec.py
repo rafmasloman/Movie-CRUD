ffa3b29f2f1b93403ee9f85fb3354fd782f0a8b8
rafmasloman <raflymasloman20@gmail.com>
2021-09-16 10:50:07 +0800

    menambah tambah film

--- dbConnec.py
+++ dbConnec.py
@@ -6,7 +6,8 @@ def main() :
     while(check):
       print(">>>> Selamat Datang di Movie List Python <<<<")
       print("1. Daftar Film")
-      print("2. Keluar")
+      print("2. Tambah Film")
+      print("3. Keluar")
       chooseMenu(check)
       enter_app = input("Ingin Keluar ? Yes/No : ")
       if(enter_app == 'yes'):
@@ -21,6 +22,8 @@ def chooseMenu(check) :
       print("berikut adalah daftar film")
       seefilm()
     if(int(pilih) == 2):
+      addMovie()
+    if(int(pilih) == 3):
       check = False
 
 def seefilm() :
@@ -33,6 +36,18 @@ def seefilm() :
     for movie in movies:
           print('{}.{}'.format(movie[0],movie[1]))
 
+def addMovie():
+      name = input("Nama Film : ")
+      genre = input("Genre : ")
+      db = database()
+      cursor = db.cursor()
+
+      sql = """INSERT INTO movies (name,genre)
+              VALUES (%s,%s) """
+      values = (name,genre)
+
+      cursor.execute(sql, values)
+      db.commit()
 
 def database() :
     db = mysql.connector.connect(
